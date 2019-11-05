8d2440c5ad999bfa6a0c101e44a5e2d610a4f414
troytjh <troytjh98@gmail.com>
2019-11-04 22:53:58 -0600

    11/04/19

--- builder/frameworks/energia.py
+++ builder/frameworks/energia.py
@@ -38,6 +38,7 @@ env.Append(
         join(platform.get_package_dir("toolchain-timsp432"),"lib","gcc","msp432","8.2.1","include"),
         join(platform.get_package_dir("toolchain-timsp432"),"arm","include"),
         join(platform.get_package_dir("toolchain-timsp432"),"arm","src"),
+        join(FRAMEWORK_DIR,"system","kernel","tirtos","packages","gnu","targets","arm"),
         join(FRAMEWORK_DIR,"system","kernel","tirtos","packages"),
         join(FRAMEWORK_DIR,"system","source")
     ],
