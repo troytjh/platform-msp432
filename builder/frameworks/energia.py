36872ac0b7b149345185a168b873b2abe95e5898
troytjh <troytjh98@gmail.com>
2019-11-05 13:37:53 -0600

    11/05/19

--- builder/frameworks/energia.py
+++ builder/frameworks/energia.py
@@ -35,7 +35,7 @@ env.Append(
         join(variants_dir, board.get("build.variant")),
         join(platform.get_package_dir("toolchain-timsp432"), "msp432", "include"),
         join(platform.get_package_dir("toolchain-timsp432"), "lib","gcc","msp432","8.2.1","include"),
-        join(platform.get_package_dir("toolchain-timsp432"), "lib","gcc","msp432","8.2.1","include-fixed"),
+        #join(platform.get_package_dir("toolchain-timsp432"), "lib","gcc","msp432","8.2.1","include-fixed"),
         #join(platform.get_package_dir("toolchain-timsp432"), "lib", "gcc","msp432","8.2.1"),
         #join(platform.get_package_dir("toolchain-timsp432"),"lib","gcc","msp432","8.2.1","include"),
         #join(platform.get_package_dir("toolchain-timsp432"),"arm","include"),
