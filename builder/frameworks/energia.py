8343e2d10ca8758c00b0530ad8e6a2be1095d60b
troytjh <troytjh98@gmail.com>
2019-11-06 16:44:05 -0600

    11/06/19

--- builder/frameworks/energia.py
+++ builder/frameworks/energia.py
@@ -41,6 +41,7 @@ env.Append(
         join(platform.get_package_dir("toolchain-timsp432"), "arm_compiler","lib","gcc","arm-none-eabi","6.2.1","include"),
         join(platform.get_package_dir("toolchain-timsp432"), "arm_compiler","arm-none-eabi","include"),
         join(platform.get_package_dir("toolchain-timsp432"),"arm","include"),
+        join(platform.get_package_dir("toolchain-timsp432"),"arm","include","CMSIS"),
         #join(platform.get_package_dir("toolchain-timsp432"), "lib","gcc","msp432","8.2.1","include"),
         #join(platform.get_package_dir("toolchain-timsp432"), "lib","gcc","msp432","8.2.1","include-fixed"),
         #join(platform.get_package_dir("toolchain-timsp432"), "lib", "gcc","msp432","8.2.1"),
