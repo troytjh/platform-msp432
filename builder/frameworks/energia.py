2f2a12417611df799032b4339c47dad7ddf99a51
troytjh <troytjh98@gmail.com>
2019-11-08 15:01:57 -0600

    11/08/19

--- builder/frameworks/energia.py
+++ builder/frameworks/energia.py
@@ -80,4 +80,9 @@ libs.append(env.BuildLibrary(
     join(FRAMEWORK_DIR, "system","source","ti","drivers"),
 ))
 
+libs.append(env.BuildLibrary(
+    join("$BUILD_DIR", "msp432p4xx"),
+    join(FRAMEWORK_DIR, "system","source","ti","devices","msp432p4xx","driverlib"),
+))
+
 env.Append(LIBS=libs)
