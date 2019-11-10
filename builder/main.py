d460afcdf2d4054b81edb16fc0f82bd8235f1b08
troytjh <troytjh98@gmail.com>
2019-11-10 17:53:48 -0600

    11/10/19

--- builder/main.py
+++ builder/main.py
@@ -88,7 +88,7 @@ env.Append(
         "-mfloat-abi=hard", 
         "-mfpu=fpv4-sp-d16", 
         "--specs=nosys.specs",
-        "--specs=rdimon.specs",
+        "--specs=rdpmon.specs",
         "--specs=nano.specs",
         "-Wl,--check-sections",
         "-Wl,--gc-sections",
