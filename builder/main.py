6af03435399a760e98b01a965c09becd422e048c
troytjh <troytjh98@gmail.com>
2019-11-11 21:31:31 -0600

    11/11/19

--- builder/main.py
+++ builder/main.py
@@ -46,7 +46,7 @@ env.Replace(
 )
 
 env.Append(
-    ASFLAGS=["-B platform.get_package_dir(toolchain-timsp432)/arm_compiler/lib/gcc/arm-none-eabi/6.2.1","-c","-x", "assembler-with-cpp"],
+    ASFLAGS=["-x", "assembler-with-cpp"],
 
     CCFLAGS=[
         "-B platform.get_package_dir(toolchain-timsp432)/arm_compiler/bin",
