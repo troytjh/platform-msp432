173134149a1b471b6fba6078c0b4ca06cd8b8c19
troytjh <troytjh98@gmail.com>
2019-11-11 22:45:37 -0600

    11/11/19

--- builder/main.py
+++ builder/main.py
@@ -49,8 +49,8 @@ env.Append(
     ASFLAGS=["-x", "assembler-with-cpp"],
 
     CCFLAGS=[
-        "-B platform.get_package_dir(toolchain-timsp432)/bin",
-        "-B platform.get_package_dir(toolchain-timsp432)/lib/gcc/arm-none-eabi/6.2.1",
+        #"-B platform.get_package_dir(toolchain-timsp432)/bin",
+        #"-B platform.get_package_dir(toolchain-timsp432)/lib/gcc/arm-none-eabi/6.2.1",
         "-c",
         "-Os",
         "-ffunction-sections",  # place each function in its own section
