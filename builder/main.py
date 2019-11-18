5c90ed8520a33f906e128fad1c212ba3e2b6094e
troytjh <troytjh98@gmail.com>
2019-11-17 18:45:09 -0600

    11/17/19

--- builder/main.py
+++ builder/main.py
@@ -95,6 +95,7 @@ env.Append(
         "-march=armv7e-m",
         "-mfloat-abi=hard", 
         "-mfpu=fpv4-sp-d16", 
+        "-nostartfile"
         "--specs=nosys.specs",
         "-Wl,--check-sections",
         "-Wl,--gc-sections",
